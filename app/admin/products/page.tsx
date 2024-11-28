4885d4a9edd013ccf0ec6aba589a1d3d193f2799
Anatolyie <anatolii.boboc96@gmail.com>
2024-11-28 22:35:22 +0100

    form component for admin create page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductsPage() {
+  return <div>adin ProductsPage</div>;
+}
+
+export default ProductsPage;
---
+++ components/form/CheckboxInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CheckboxInput() {
+  return <div>CheckboxInput</div>;
+}
+
+export default CheckboxInput;
