d538c1af75ee501bcb94a51c13653794b39cfd6e
Anatolyie <anatolii.boboc96@gmail.com>
2024-11-28 23:24:54 +0100

    form container added

--- components/form/CheckboxInput.tsx
+++ components/form/CheckboxInput.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
 
-function CheckboxInput() {
-  return <div>CheckboxInput</div>;
+import { Checkbox } from "@/components/ui/checkbox";
+
+type CheckboxInputProps = {
+  name: string;
+  label: string;
+  defaultChecked?: boolean;
+};
+
+function CheckboxInput({
+  name,
+  label,
+  defaultChecked = false,
+}: CheckboxInputProps) {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id={name} name={name} defaultChecked={defaultChecked} />
+      <label
+        htmlFor={name}
+        className="text-sm leading-none capitalize peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {label}
+      </label>
+    </div>
+  );
 }
-
 export default CheckboxInput;
