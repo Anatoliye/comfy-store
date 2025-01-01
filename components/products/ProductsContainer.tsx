81aee7da63ac0e4442dee0bf44e7c6afcff3c7b9
Anatolyie <anatolii.boboc96@gmail.com>
2025-01-01 20:15:49 +0100

    Fix ESLint unused variable errors

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -1,6 +1,6 @@
 import ProductsGrid from "./ProductsGrid";
 import ProductsList from "./ProductsList";
-import { LuLayoutGrid, LuList } from "react-icons/lu";
+import { LuLayoutGrid } from "react-icons/lu";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { fetchAllProducts } from "@/utils/actions";
